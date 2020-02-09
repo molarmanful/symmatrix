5877ed4a078da72e86f44bd2f4808c2a33a77acd
Benjamin Pang <benjinpang@gmail.com>
2020-02-09 15:43:35 +0400

    async looping to prevent hang

--- js/main.js
+++ js/main.js
@@ -5,17 +5,22 @@ window.ENV = new Env(out, innerWidth, innerHeight)
 
 onload = _=>{
   ENV.add(
-    new Plant(ENV, 0, 0, '#00ff00', 3, 10, 1)
+    new Plant(ENV, innerWidth / 2, innerHeight / 2, '#00ff00', 3, 10, 1)
   )
 
   ENV.cells.map(a=> a.display())
 
-  onkeydown = e=>{
-    if(e.key == ' '){
+  // onkeydown = e=>{
+  //   if(e.key == ' '){
+  //     ENV.step()
+  //   }
+  // }
+
+  async function loop(){
+    while(true){
+      await new Promise(r=> setTimeout(r, 100))
       ENV.step()
     }
   }
-  // setInterval(_=>{
-  //   ENV.step()
-  // }, 1000)
+  loop()
 }
